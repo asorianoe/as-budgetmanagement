0f28605ed8a762df1e77f85d7a8663f7e1cc8f51
Alex Soriano <alexander.sorianoestupinian@telus.com>
2023-01-14 13:15:40 -0600

    [BM] -Updating DB init script

--- bm-database/init/dbcreation.sql
+++ bm-database/init/dbcreation.sql
@@ -1,32 +1,44 @@
-ALTER SESSION SET CONTAINER=XEPDB1;
+ALTER SESSION SET CONTAINER=xepdb1;
 
 CREATE TABLE bmuser.bm_currency
 (
-	curr_id varchar2(3) NOT NULL, 
-	curr_desc varchar2(20),
-	curr_usd_exc_rate number(10,2), 
+	curr_id VARCHAR2(3) NOT NULL, 
+	curr_desc VARCHAR2(20),
+	curr_usd_exc_rate NUMBER(10,2), 
 	CONSTRAINT bm_currency_pk PRIMARY KEY(curr_id) ENABLE 
 );
 
-CREATE TABLE  bmuser.BM_TRANSACTIONS_TYPE
+CREATE TABLE  bmuser.bm_transactions_type
 (
-	tx_type varchar2(3) NOT NULL , 
-	tx_type_desc varchar2(20),
-	CONSTRAINT BM_TRANSACTIONS_TYPE_PK PRIMARY KEY(tx_type) ENABLE 
+	tx_type VARCHAR2(3) NOT NULL , 
+	tx_type_desc VARCHAR2(20),
+	CONSTRAINT bm_transactions_type_pk PRIMARY KEY(tx_type) ENABLE 
 );
 
-CREATE TABLE  bmuser.BM_TRANSACTIONS_CAT
+CREATE TABLE  bmuser.bm_transactions_cat
 (
-	tx_cat varchar2(3) NOT NULL, 
-	tx_cat_desc varchar2(20),
-	tx_type varchar2(3) NOT NULL,
-	CONSTRAINT BM_TRANSACTIONS_CAT_PK PRIMARY KEY(tx_cat) ENABLE 
+	tx_cat VARCHAR2(3) NOT NULL, 
+	tx_cat_desc VARCHAR2(20),
+	tx_type VARCHAR2(3) NOT NULL,
+	tx_visible VARCHAR(1) DEFAULT 'Y' NOT NULL,
+	CONSTRAINT bm_transactions_cat_pk PRIMARY KEY(tx_cat) ENABLE 
 );
 
-ALTER TABLE bmuser.BM_TRANSACTIONS_CAT
-	ADD CONSTRAINT BM_TRANSACTIONS_CAT_FK1 FOREIGN KEY (tx_type)
-	REFERENCES bmuser.BM_TRANSACTIONS_TYPE(tx_type)
+ALTER TABLE bmuser.bm_transactions_cat
+	ADD CONSTRAINT bm_transactions_cat_fk1 FOREIGN KEY (tx_type)
+	REFERENCES bmuser.bm_transactions_type(tx_type)
 ENABLE;
+
+CREATE TABLE bmuser.bm_user (
+	user_id VARCHAR2(10) NOT NULL,
+	user_pass VARCHAR2(100) NOT NULL,
+	user_token VARCHAR2(1000) NOT NULL,
+	user_first_name VARCHAR2(100) NOT NULL,
+	user_last_name VARCHAR2(100) NOT NULL,
+	create_date DATE DEFAULT sysdate NOT NULL,
+	update_date DATE DEFAULT sysdate NOT NULL,
+	CONSTRAINT bm_user_pk PRIMARY KEY(user_id) ENABLE
+);
 
 CREATE TABLE bmuser.bm_account
 (
@@ -34,79 +46,83 @@ CREATE TABLE bmuser.bm_account
 	account_id NUMBER NOT NULL,
 	ALIAS VARCHAR2(20) NOT NULL,
 	balance NUMBER DEFAULT 0 NOT NULL,
-	currency varchar2(3) NOT NULL, 
+	currency VARCHAR2(3) NOT NULL, 
 	create_date DATE DEFAULT sysdate NOT NULL,
 	update_date DATE DEFAULT sysdate NOT NULL,
-	CONSTRAINT BM_ACCOUNT_PK PRIMARY KEY(ACCOUNT_ID) ENABLE
+	CONSTRAINT bm_account_pk PRIMARY KEY(account_id) ENABLE
 );
 
-ALTER TABLE bmuser.BM_ACCOUNT
-	ADD CONSTRAINT BM_ACCOUNT_FK1 FOREIGN KEY (currency)
-	REFERENCES bmuser.BM_CURRENCY(CURR_ID)
+ALTER TABLE bmuser.bm_account
+	ADD CONSTRAINT bm_account_fk1 FOREIGN KEY (currency)
+	REFERENCES bmuser.bm_currency(curr_id)
 ENABLE;
 
+ALTER TABLE bmuser.bm_user
+	ADD CONSTRAINT bm_account_fk2 FOREIGN KEY (user_id)
+	REFERENCES bmuser.bm_user(user_id)
+ENABLE;
 
-CREATE TABLE bmuser.BM_TRANSACTION 
+CREATE TABLE bmuser.bm_transaction 
 (
-	tx_id 				number not null, 
-  ACCOUNT_ID 		NUMBER NOT NULL, 
-	tx_type  			varchar2(3) NOT NULL, 
-	tx_cat 				varchar2(3) NOT NULL, 
-	tx_org_amount number(10,2) not null, 
-	tx_currency 	varchar2(3) not null, 
-	tx_exg_amount number(10,2) not null,
-	tx_date       date default sysdate not null,
-	CONSTRAINT BM_TRANSACTION PRIMARY KEY(tx_id) ENABLE
+	tx_id 				NUMBER NOT NULL, 
+  account_id 		NUMBER NOT NULL, 
+	tx_type  			VARCHAR2(3) NOT NULL, 
+	tx_cat 				VARCHAR2(3) NOT NULL, 
+	tx_org_amount NUMBER(10,2) NOT NULL, 
+	tx_currency 	VARCHAR2(3) NOT NULL, 
+	tx_exg_amount NUMBER(10,2) NOT NULL,
+	tx_date       DATE DEFAULT sysdate NOT NULL,
+	CONSTRAINT bm_transaction PRIMARY KEY(tx_id) ENABLE
 );
 
-ALTER TABLE bmuser.BM_TRANSACTION
-	ADD CONSTRAINT BM_TRANSACTION_FK1 FOREIGN KEY (TX_CURRENCY)
-	REFERENCES bmuser.BM_CURRENCY(CURR_ID)
+ALTER TABLE bmuser.bm_transaction
+	ADD CONSTRAINT bm_transaction_fk1 FOREIGN KEY (tx_currency)
+	REFERENCES bmuser.bm_currency(curr_id)
 	ENABLE;
 
-ALTER TABLE bmuser.BM_TRANSACTION
-	ADD CONSTRAINT BM_TRANSACTION_FK2 FOREIGN KEY(ACCOUNT_ID)
-	REFERENCES bmuser.BM_ACCOUNT(ACCOUNT_ID)
+ALTER TABLE bmuser.bm_transaction
+	ADD CONSTRAINT bm_transaction_fk2 FOREIGN KEY(account_id)
+	REFERENCES bmuser.bm_account(account_id)
 	ENABLE;
 
-ALTER TABLE bmuser.BM_TRANSACTION
-	ADD CONSTRAINT BM_TRANSACTION_FK3 FOREIGN KEY(TX_TYPE)
-	REFERENCES bmuser.BM_TRANSACTIONS_TYPE(TX_TYPE)
+ALTER TABLE bmuser.bm_transaction
+	ADD CONSTRAINT bm_transaction_fk3 FOREIGN KEY(tx_type)
+	REFERENCES bmuser.bm_transactions_type(tx_type)
 	ENABLE;
 
-ALTER TABLE bmuser.BM_TRANSACTION
-	ADD CONSTRAINT BM_TRANSACTION_FK4 FOREIGN KEY(TX_CAT)
-	REFERENCES bmuser.BM_TRANSACTIONS_CAT(TX_CAT)
+ALTER TABLE bmuser.bm_transaction
+	ADD CONSTRAINT bm_transaction_fk4 FOREIGN KEY(tx_cat)
+	REFERENCES bmuser.bm_transactions_cat(tx_cat)
 	ENABLE;
 
-CREATE SEQUENCE bmuser.BM_TRANSACTION_SEQ INCREMENT BY 1 START WITH 1 NOCACHE;
-CREATE SEQUENCE bmuser.bm_account_SEQ  INCREMENT BY 1 START WITH 1 NOCACHE;
+CREATE SEQUENCE bmuser.bm_transaction_seq INCREMENT BY 1 START WITH 1 NOCACHE;
+CREATE SEQUENCE bmuser.bm_account_seq  INCREMENT BY 1 START WITH 1 NOCACHE;
 
 
-create or replace view bmuser.conv_rate as
-	select org_cur,new_cur,conv_rate from (
-		select 
+CREATE OR REPLACE VIEW bmuser.conv_rate AS
+	SELECT org_cur,new_cur,conv_rate FROM (
+		SELECT 
 			from_cur.curr_id org_cur, to_cur.curr_id new_cur, to_cur.curr_usd_exc_rate/from_cur.curr_usd_exc_rate conv_rate
-		from  bmuser.bm_currency from_cur,  bmuser.bm_currency to_cur 
+		FROM  bmuser.bm_currency from_cur,  bmuser.bm_currency to_cur 
 )
 ;
 
-create or replace trigger bmuser.BM_TRANSACTION_TRG  
-   before insert on BMUSER.BM_TRANSACTION 
-   for each row 
-declare
-	to_curr varchar2(3);
-begin  
-   if inserting then 
-			select bmuser.BM_TRANSACTION_SEQ.nextval into :NEW.TX_ID from dual;
-			SELECT currency into to_curr from bmuser.bm_account where account_id = :NEW.ACCOUNT_ID;
-			SELECT CONV_RATE * :NEW.tx_org_amount into :NEW.tx_exg_amount from bmuser.conv_rate where org_cur = :NEW.tx_currency and new_cur = to_curr;
+CREATE OR REPLACE TRIGGER bmuser.bm_transaction_trg  
+   BEFORE INSERT ON bmuser.bm_transaction 
+   FOR EACH ROW 
+DECLARE
+	to_curr VARCHAR2(3);
+BEGIN  
+   IF inserting THEN 
+			SELECT bmuser.bm_transaction_seq.NEXTVAL INTO :NEW.tx_id FROM dual;
+			SELECT currency INTO to_curr FROM bmuser.bm_account WHERE account_id = :NEW.account_id;
+			SELECT conv_rate * :NEW.tx_org_amount INTO :NEW.tx_exg_amount FROM bmuser.conv_rate WHERE org_cur = :NEW.tx_currency AND new_cur = to_curr;
 			UPDATE bmuser.bm_account
 				SET balance = balance + :NEW.tx_exg_amount
 			WHERE 
-				account_id = :NEW.ACCOUNT_ID;
-   end if; 
-end;
+				account_id = :NEW.account_id;
+   END IF; 
+END;
 /
 
 CREATE OR REPLACE PROCEDURE bmuser.bm_transfer 
@@ -116,26 +132,43 @@ BEGIN
 	INSERT INTO bmuser.bm_transaction (account_id, tx_type, tx_cat, tx_org_amount, tx_currency) VALUES (to_acc, 'EXP', 'OTH', amount, currency);
 END bm_transfer;
 /
+
+-- CREATE FUNCTION
+CREATE OR REPLACE FUNCTION bmuser.API_TOKEN(PSECRET VARCHAR2) RETURN VARCHAR2
+IS
+  	VRESULT VARCHAR2(4000);
+BEGIN
+	SELECT UTL_RAW.CAST_TO_VARCHAR2(UTL_I18N.STRING_TO_RAW(STANDARD_HASH(PSECRET, 'MD5'), 'AL32UTF8')) INTO VRESULT from dual;
+	RETURN VRESULT;
+END API_TOKEN;
+/
+
 /*DATA*/
 
-INSERT INTO BMUSER.BM_CURRENCY (CURR_ID, CURR_DESC, CURR_USD_EXC_RATE) VALUES ('USD', 'U.S Dollar', '1');
-INSERT INTO BMUSER.BM_CURRENCY (CURR_ID, CURR_DESC, CURR_USD_EXC_RATE) VALUES ('COP', 'Colombiam Peso', 4729.76);
-INSERT INTO BMUSER.BM_CURRENCY (CURR_ID, CURR_DESC, CURR_USD_EXC_RATE) VALUES ('MEX', 'Mexican Peso', 19.39);
-INSERT INTO BMUSER.BM_CURRENCY (CURR_ID, CURR_DESC, CURR_USD_EXC_RATE) VALUES ('CRC', 'Costa Rican Colón', 581.85);
-INSERT INTO BMUSER.BM_CURRENCY (CURR_ID, CURR_DESC, CURR_USD_EXC_RATE) VALUES ('EUR', 'Euro', 0.94);
-INSERT INTO BMUSER.BM_CURRENCY (CURR_ID, CURR_DESC, CURR_USD_EXC_RATE) VALUES ('GBP', 'Pound sterling', 0.83);
+INSERT INTO bmuser.bm_currency (curr_id, curr_desc, curr_usd_exc_rate) VALUES ('USD', 'U.S Dollar', '1');
+INSERT INTO bmuser.bm_currency (curr_id, curr_desc, curr_usd_exc_rate) VALUES ('COP', 'Colombiam Peso', 4729.76);
+INSERT INTO bmuser.bm_currency (curr_id, curr_desc, curr_usd_exc_rate) VALUES ('MEX', 'Mexican Peso', 19.39);
+INSERT INTO bmuser.bm_currency (curr_id, curr_desc, curr_usd_exc_rate) VALUES ('CRC', 'Costa Rican Colón', 581.85);
+INSERT INTO bmuser.bm_currency (curr_id, curr_desc, curr_usd_exc_rate) VALUES ('EUR', 'Euro', 0.94);
+INSERT INTO bmuser.bm_currency (curr_id, curr_desc, curr_usd_exc_rate) VALUES ('GBP', 'Pound sterling', 0.83);
 
 
-INSERT INTO BMUSER.BM_TRANSACTIONS_TYPE (tx_type, TX_TYPE_DESC) VALUES ('EXP', 'Expense');
-INSERT INTO BMUSER.BM_TRANSACTIONS_TYPE (tx_type, TX_TYPE_DESC) VALUES ('INC', 'Income');
-INSERT INTO BMUSER.BM_TRANSACTIONS_TYPE (tx_type, TX_TYPE_DESC) VALUES ('TRA', 'Transfer');
+INSERT INTO bmuser.bm_transactions_type (tx_type, tx_type_desc) VALUES ('EXP', 'Expense');
+INSERT INTO bmuser.bm_transactions_type (tx_type, tx_type_desc) VALUES ('INC', 'Income');
+INSERT INTO bmuser.bm_transactions_type (tx_type, tx_type_desc) VALUES ('TRA', 'Transfer');
 
-INSERT INTO BMUSER.BM_TRANSACTIONS_CAT (TX_CAT, TX_CAT_DESC, TX_TYPE) VALUES ('HOU', 'Hosing ', 'EXP');
-INSERT INTO BMUSER.BM_TRANSACTIONS_CAT (TX_CAT, TX_CAT_DESC, TX_TYPE) VALUES ('FOO', 'Food', 'EXP');
-INSERT INTO BMUSER.BM_TRANSACTIONS_CAT (TX_CAT, TX_CAT_DESC, TX_TYPE) VALUES ('ENT', 'Entretaining', 'EXP');
-INSERT INTO BMUSER.BM_TRANSACTIONS_CAT (TX_CAT, TX_CAT_DESC, TX_TYPE) VALUES ('SAL', 'Salary', 'INC');
-INSERT INTO BMUSER.BM_TRANSACTIONS_CAT (TX_CAT, TX_CAT_DESC, TX_TYPE) VALUES ('OTH', 'Others', 'INC');
-INSERT INTO BMUSER.BM_TRANSACTIONS_CAT (TX_CAT, TX_CAT_DESC, TX_TYPE) VALUES ('OTH', 'Others', 'EXP');
+INSERT INTO bmuser.bm_transactions_cat (tx_cat, tx_cat_desc, tx_type) VALUES ('HOU', 'Hosing ', 'EXP');
+INSERT INTO bmuser.bm_transactions_cat (tx_cat, tx_cat_desc, tx_type) VALUES ('FOO', 'Food', 'EXP');
+INSERT INTO bmuser.bm_transactions_cat (tx_cat, tx_cat_desc, tx_type) VALUES ('ENT', 'Entretaining', 'EXP');
+INSERT INTO bmuser.bm_transactions_cat (tx_cat, tx_cat_desc, tx_type) VALUES ('SAL', 'Salary', 'INC');
+INSERT INTO bmuser.bm_transactions_cat (tx_cat, tx_cat_desc, tx_type) VALUES ('SLS', 'Sales', 'INC');
+INSERT INTO bmuser.bm_transactions_cat (tx_cat, tx_cat_desc, tx_type) VALUES ('OTE', 'Other Expenses', 'EXP');
+INSERT INTO bmuser.bm_transactions_cat (tx_cat, tx_cat_desc, tx_type) VALUES ('OTI', 'Other Income', 'EXP');
+INSERT INTO bmuser.bm_transactions_cat (tx_cat, tx_cat_desc, tx_type, tx_visible) VALUES ('TRI', 'Tranfer Increase', 'INC', 'N');
+INSERT INTO bmuser.bm_transactions_cat (tx_cat, tx_cat_desc, tx_type, tx_visible) VALUES ('TRE', 'Tranfer Expense', 'EXP', 'N');
+
+INSERT INTO bmuser.bm_user (user_id, user_mail, user_pass, user_token, user_first_name, user_last_name) VALUES ('asoriano', 'asoriano', 'NoToken', 'Alex', 'Soriano');
+
 
 COMMIT;
 
